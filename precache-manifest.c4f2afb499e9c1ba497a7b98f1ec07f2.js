f835af5b809278eae784c968598630f4989ddb01
Elmekkimahmoud <59558915+Elmekkimahmoud@users.noreply.github.com>
2020-02-06 16:11:24 +0100

    Updates

---
+++ precache-manifest.c4f2afb499e9c1ba497a7b98f1ec07f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d8f93c0bcfce6de6eed0d1c423b27f",
+    "url": "/REACT-NAVMENU/index.html"
+  },
+  {
+    "revision": "496a3298fa25b718f782",
+    "url": "/REACT-NAVMENU/static/css/main.e93d220c.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/REACT-NAVMENU/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/REACT-NAVMENU/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496a3298fa25b718f782",
+    "url": "/REACT-NAVMENU/static/js/main.3af437a9.chunk.js"
+  },
+  {
+    "revision": "f724212ab024b4232195",
+    "url": "/REACT-NAVMENU/static/js/runtime-main.fa43e775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/REACT-NAVMENU/static/media/logo.5d5d9eef.svg"
+  }
+]);
